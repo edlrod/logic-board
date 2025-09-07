5364178cf5c5621c99d15806f4770b770588ffb8
edlessz <42257904+edlessz@users.noreply.github.com>
2025-09-06 21:29:23 -0400

    chore: use biomejs

--- src/Board.ts
+++ src/Board.ts
@@ -1,10 +1,10 @@
-import Chip from "./Chip";
+import type Chip from "./Chip";
 
 export default class Board {
-  public name: string | null = null;
-  public chips: Chip[] = [];
-  constructor(name: string) {
-    this.name = null;
-    if (name) this.name = name.toUpperCase();
-  }
+	public name: string | null = null;
+	public chips: Chip[] = [];
+	constructor(name: string) {
+		this.name = null;
+		if (name) this.name = name.toUpperCase();
+	}
 }
--- src/Camera.ts
+++ src/Camera.ts
@@ -1,57 +1,57 @@
 import type { Vector2 } from "./types";
 
 export default class Camera {
-  static position: Vector2 = {
-    x: 0,
-    y: 0,
-  };
-  static PPU = 32;
+	static position: Vector2 = {
+		x: 0,
+		y: 0,
+	};
+	static PPU = 32;
 
-  static minPPU = 8;
-  static maxPPU = 256;
+	static minPPU = 8;
+	static maxPPU = 256;
 
-  private static canvasContext: HTMLCanvasElement | null = null;
+	private static canvasContext: HTMLCanvasElement | null = null;
 
-  static initialize(canvasContext?: HTMLCanvasElement) {
-    if (canvasContext) this.canvasContext = canvasContext;
-  }
+	static initialize(canvasContext?: HTMLCanvasElement) {
+		if (canvasContext) Camera.canvasContext = canvasContext;
+	}
 
-  static zoomAtPosition(zoomAmount: number, zoomPosition: Vector2) {
-    const centeredMousePosition = {
-      x: (zoomPosition.x - this.position.x) * this.PPU,
-      y: (zoomPosition.y - this.position.y) * this.PPU,
-    };
-    this.position.x += centeredMousePosition.x / this.PPU;
-    this.position.y += centeredMousePosition.y / this.PPU;
+	static zoomAtPosition(zoomAmount: number, zoomPosition: Vector2) {
+		const centeredMousePosition = {
+			x: (zoomPosition.x - Camera.position.x) * Camera.PPU,
+			y: (zoomPosition.y - Camera.position.y) * Camera.PPU,
+		};
+		Camera.position.x += centeredMousePosition.x / Camera.PPU;
+		Camera.position.y += centeredMousePosition.y / Camera.PPU;
 
-    this.PPU += zoomAmount;
-    this.PPU = Math.min(this.PPU, this.maxPPU);
-    this.PPU = Math.max(this.PPU, this.minPPU);
+		Camera.PPU += zoomAmount;
+		Camera.PPU = Math.min(Camera.PPU, Camera.maxPPU);
+		Camera.PPU = Math.max(Camera.PPU, Camera.minPPU);
 
-    this.position.x -= centeredMousePosition.x / this.PPU;
-    this.position.y -= centeredMousePosition.y / this.PPU;
-  }
+		Camera.position.x -= centeredMousePosition.x / Camera.PPU;
+		Camera.position.y -= centeredMousePosition.y / Camera.PPU;
+	}
 
-  public static screenToWorld(originalPosition: Vector2): Vector2 {
-    if (!this.canvasContext)
-      throw "A canvas context is required to use this screenToWorld()";
-    const centeredPosition: Vector2 = {
-      x: originalPosition.x - this.canvasContext.width / 2,
-      y: originalPosition.y - this.canvasContext.height / 2,
-    };
-    return {
-      x: this.position.x + centeredPosition.x / this.PPU,
-      y: this.position.y + centeredPosition.y / this.PPU,
-    };
-  }
+	public static screenToWorld(originalPosition: Vector2): Vector2 {
+		if (!Camera.canvasContext)
+			throw "A canvas context is required to use this screenToWorld()";
+		const centeredPosition: Vector2 = {
+			x: originalPosition.x - Camera.canvasContext.width / 2,
+			y: originalPosition.y - Camera.canvasContext.height / 2,
+		};
+		return {
+			x: Camera.position.x + centeredPosition.x / Camera.PPU,
+			y: Camera.position.y + centeredPosition.y / Camera.PPU,
+		};
+	}
 
-  public static rotatePoint(point: Vector2, angle: number): Vector2 {
-    angle %= Math.PI * 2;
-    const cos = Math.cos(angle * -1);
-    const sin = Math.sin(angle * -1);
-    return {
-      x: cos * point.x + sin * point.y,
-      y: cos * point.y - sin * point.x,
-    };
-  }
+	public static rotatePoint(point: Vector2, angle: number): Vector2 {
+		angle %= Math.PI * 2;
+		const cos = Math.cos(angle * -1);
+		const sin = Math.sin(angle * -1);
+		return {
+			x: cos * point.x + sin * point.y,
+			y: cos * point.y - sin * point.x,
+		};
+	}
 }
