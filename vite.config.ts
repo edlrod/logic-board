53cea8ccb455249ef90762262614eb8b70e02d5b
edlrod <42257904+edlrod@users.noreply.github.com>
2026-04-27 00:29:49 -0400

    chore: add commitlint configuration and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/logic-board/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
